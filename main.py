f2662f7fec72679be540054b74d3237629a6dac8
Kevin Moreno <kevin@MacBook-Air-de-USER.local>
2023-06-11 14:24:13 -0500

    add page 404

--- main.py
+++ main.py
@@ -8,45 +8,49 @@ password = "root"
 
 @app.route('/')
 def index():
-    return render_template('index.html', todos=todos)
-
-@app.route('/login')
-def login():
     return render_template('login.html')
 
-@app.route('/logincomp', methods=['GET', 'POST'])
-def logincomp():
+@app.route('/app')
+def todo():
+    return render_template('index.html', todos=todos)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     userinp = request.form['uname']
     passinp = request.form['psw']
     if username == userinp and password==passinp:
-        return redirect(url_for('index'))
+        return redirect(url_for('todo'))
     else:
-        return render_template('login.html')
+        return redirect(url_for('index'))
 
 @app.route('/add', methods=['POST'])
 def add():
     todo = request.form['todo']
     todos.append({'task': todo, 'done': False})
-    return redirect(url_for('index'))
+    return redirect(url_for('todo'))
 
 @app.route('/edit/<int:index>', methods=['GET', 'POST'])
 def edit(index):
     todo = todos[index]
     if request.method == 'POST':
         todo['task'] = request.form['todo']
-        return redirect(url_for('index'))
+        return redirect(url_for('todo'))
     else:
         return render_template('edit.html', todo=todo, index=index)
 
 @app.route('/check/<int:index>')
 def check(index):
     todos[index]['done'] = not todos[index]['done']
-    return redirect(url_for('index'))
+    return redirect(url_for('todo'))
 
 @app.route('/delete/<int:index>')
 def delete(index):
     del todos[index]
-    return redirect(url_for('index'))
+    return redirect(url_for('todo'))
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('custom_page.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
